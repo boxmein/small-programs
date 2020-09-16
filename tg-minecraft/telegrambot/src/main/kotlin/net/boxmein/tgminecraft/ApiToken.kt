8b4b7100d7ca813356573708ea2a34c85f2c15b4
boxmein <wsdf294@gmail.com>
2020-09-16 12:15:49 +0300

    API token from env, for now

--- tg-minecraft/telegrambot/src/main/kotlin/net/boxmein/tgminecraft/ApiToken.kt
+++ tg-minecraft/telegrambot/src/main/kotlin/net/boxmein/tgminecraft/ApiToken.kt
@@ -10,8 +10,10 @@ import kotlinx.serialization.json.content
 const val secret = "tgminebot/auth"
 
 class ApiTokenFetcher {
-  val apiToken: String
+  val apiToken: String = System.getenv("TELEGRAM_BOT_TOKEN")
 
+  /*
+  AWS Secrets Manager
   init {
     val client = SecretsManagerClient.builder().region(Region.EU_NORTH_1).build()
     val secret = client.getSecretValue(GetSecretValueRequest.builder().secretId(secret).build())
@@ -19,4 +21,5 @@ class ApiTokenFetcher {
     val result = parser.parseJson(secret.secretString()).jsonObject
     apiToken = result["apiToken"]!!.content
   }
+  */
 }
