c2eb90b4a881e628946bf901c82cc0e4bf4662e3
boxmein <wsdf294@gmail.com>
2020-09-19 23:59:33 +0300

    Add assertions but cant use them

--- tg-minecraft/telegrambot/src/main/kotlin/net/boxmein/tgminecraft/Main.kt
+++ tg-minecraft/telegrambot/src/main/kotlin/net/boxmein/tgminecraft/Main.kt
@@ -7,11 +7,14 @@ import com.github.kotlintelegrambot.dispatcher.command
 fun main() {
   println("Hello, mc")
 
-  val apiToken: String = if (System.getenv("APP_ENV") == "production") {
-    ApiTokenFetcher().apiToken
-  } else {
-    ""
-  }
+  val apiToken: String = ApiTokenFetcher().apiToken
+
+  assert(apiToken.length > 0)
+  assert(System.getenv("APP_ENV") != null)
+  assert(System.getenv("AWS_ACCESS_KEY_ID") != null)
+  assert(System.getenv("AWS_SECRET_ACCESS_KEY") != null)
+  assert(System.getenv("AWS_DEFAULT_REGION") != null)
+  assert(System.getenv("SERVER_AWS_INSTANCE_ID") != null)
 
   val vpsService = VPSService()
 
--- tg-minecraft/telegrambot/src/main/kotlin/net/boxmein/tgminecraft/VPSService.kt
+++ tg-minecraft/telegrambot/src/main/kotlin/net/boxmein/tgminecraft/VPSService.kt
@@ -3,11 +3,16 @@ package net.boxmein.tgminecraft
 import software.amazon.awssdk.services.ec2.Ec2Client
 import software.amazon.awssdk.services.ec2.model.StartInstancesRequest
 import software.amazon.awssdk.services.ec2.model.StopInstancesRequest
+import software.amazon.awssdk.regions.Region
 
-const val INSTANCE_ID: String = "i-03c2fe6178924d185"
+val INSTANCE_ID: String? = System.getenv("SERVER_AWS_INSTANCE_ID")
+val REGION: String = System.getenv("AWS_DEFAULT_REGION") ?: "eu-north-1"
 
 class VPSService {
-  val ec2 = Ec2Client.builder().build()
+  val ec2 = Ec2Client.builder()
+    .region(Region.of(REGION))
+    .build()
+
   fun startServer() {
     val req = StartInstancesRequest
       .builder()
