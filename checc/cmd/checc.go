def7ded05458d9ff9d05c315d23639b9a93329ac
boxmein <wsdf294@gmail.com>
2020-12-03 16:29:49 +0200

    Simple checking tool

---
+++ checc/cmd/checc.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+)
+
+func loadConfig() ([]string, error) {
+	data, err := ioutil.ReadFile("./checc.lst")
+
+	if err != nil {
+		return []string{}, err
+	}
+
+	dataStr := string(data)
+
+	return strings.Split(dataStr, "\n"), nil
+}
+
+func main() {
+	fmt.Println("checcing")
+
+	commands, err := loadConfig()
+
+	if err != nil {
+		fmt.Errorf("%s", err.Error())
+		panic("file did not load - create a ./checc.lst file")
+	}
+
+	for _, command := range commands {
+		commandObj := exec.Command("bash", "-c", command)
+		err := commandObj.Run()
+		if err != nil {
+			fmt.Printf("\x1b[31merror\x1b[0m   %s\n", command)
+		} else {
+			fmt.Printf("\x1b[32msuccess\x1b[0m %s\n", command)
+		}
+	}
+}
