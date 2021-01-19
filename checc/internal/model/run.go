02cd94130acc02bd3eb2b9d69543aca2b76dbdbf
boxmein <wsdf294@gmail.com>
2021-01-19 15:28:39 +0200

    graduated to its own repo

--- checc/internal/model/run.go
+++
@@ -1,8 +0,0 @@
-package model
-
-// Run represents one check's one Run() invocation.
-// A run most include a success/failure status and might include some logs.
-type Run interface {
-	Successful() bool
-	Logs() string
-}
