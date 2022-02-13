28b5586dbe81a176204e4560335e581d772ea933
boxmein <wsdf294@gmail.com>
2022-02-13 13:20:34 +0200

    admin repository thingy

--- redismanager/pkg/admin.py
+++ redismanager/pkg/admin.py
@@ -8,8 +8,9 @@ class AdminService:
   def report(self): 
     for user_b in self.db.list_accounts():
       user = str(user_b, 'utf8')
-      print(f"====================\nREPORT FOR {user}\n")
+      print(f"---\nREPORT FOR {user}\n")
       print(f"Quota: {self.db.read_user_quota(user)}")
       print(f"Lifetime value: {self.db.get_ltv(user)}")
       print(f"Servers: {self.db.count_servers(user)}")
       print(f"Unpaid invoices: {self.db.account_has_unpaid_invoices(user)}")
+      print("---\n")
