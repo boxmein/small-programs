7089aa7eeb0747415fb59e8f1b4bf914a1716bef
johannes.kadak <johannes.kadak@bolt.eu>
2023-11-30 16:02:55 +0200

    use buck2 to compile cursed-string-detector

--- cursed-string-detector/testutil.c
+++ cursed-string-detector/testutil.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 
-int main() {
+int main()
+{
   char data[] = "HELLOWORLD";
   printf("My PID is: %d\n", getpid());
   sleep(99999999);
